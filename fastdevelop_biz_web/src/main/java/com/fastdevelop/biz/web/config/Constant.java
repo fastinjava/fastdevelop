8fab852137c266de4be7cde728b25fdaea8b3951
leefuyong <tingting521>
2021-02-14 22:45:56 +0800

    c

--- fastdevelop_biz_web/src/main/java/com/fastdevelop/biz/web/config/Constant.java
+++ fastdevelop_biz_web/src/main/java/com/fastdevelop/biz/web/config/Constant.java
@@ -2,4 +2,9 @@ package com.fastdevelop.biz.web.config;
 
 public interface Constant {
     public static String HEADER_AUTHORIZATION = "Authorization";
+
+    public interface RedisConstant {
+        String COMMON_PREFIX = "P1F01";
+    }
+
 }
